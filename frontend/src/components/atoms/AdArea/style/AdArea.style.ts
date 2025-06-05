dc62cb2b8b4b2f235228346193b84b675726e25d
Hoyeong Yoo <crystal23733@gmail.com>
2025-06-05 16:34:13 +0900

    Feat|광고 영역 스타일 정의
    - 기본 스타일 그룹(flex)
    - 위치에 따른 스타일 그룹 정의

---
+++ frontend/src/components/atoms/AdArea/style/AdArea.style.ts
@@ -0,0 +1,17 @@
+import AdAreaProps from "../AdArea.types";
+
+/**
+ * 기본 스타일 그룹
+ */
+export const baseStyle: string =
+  "flex items-center justify-center bg-gray-100 rounded shadow text-gray-500 text-xs";
+
+/**
+ * 위치에 따른 스타일 그룹
+ */
+export const positionStyle: Record<AdAreaProps["position"], string> = {
+  top: "w-full min-h-[70px] mb-4",
+  bottom: "w-full min-h-[60px] mt-4",
+  left: "hidden lg:flex fixed top-0 left-0 h-screen w-[120px] z-40",
+  right: "hidden lg:flex fixed top-0 right-0 h-screen w-[120px] z-40",
+};
