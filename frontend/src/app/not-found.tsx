0da3ec171af35d92274f7fcf16c8202b3dc1c6a9
Hoyeong Yoo <crystal23733@gmail.com>
2025-06-13 01:24:54 +0900

    코드 포매팅

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -3,5 +3,5 @@
 import NotFoundPage from "@/components/pages/NotFoundPage/NotFoundPage";
 
 export default function NotFound() {
-  return <NotFoundPage onGoHome={() => window.location.href = "/"} />;
+  return <NotFoundPage onGoHome={() => (window.location.href = "/")} />;
 }
